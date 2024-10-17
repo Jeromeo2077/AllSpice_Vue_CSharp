d6c71fe88f5884eda41eb9c9924c5f94c17bc04f
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-17 15:31:09 -0600

    Update Recipe model with additional properties and data annotations

--- allspice_vue_csharp/server/Models/Recipe.cs
+++ allspice_vue_csharp/server/Models/Recipe.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace allspice_vue_csharp.Models;
 
 public class Recipe
@@ -5,7 +7,16 @@ public class Recipe
   public int Id { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
-  public string Title { get; set}
 
+  [MaxLength(255)]
+  public string Title { get; set; }
 
+  [MaxLength(5000)]
+  public string Instructions { get; set; }
+
+  [MaxLength(1000)]
+  public string Img { get; set; }
+
+  public string Category { get; set; }
+  public string creatorID { get; set; }
 }
