5b3478c27d61f24e7afae3a249d93a8d2686cf02
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-17 15:00:03 -0600

    Update environment variables and started Recipe schema

---
+++ allspice_vue_csharp/server/Models/Recipe.cs
@@ -0,0 +1,11 @@
+namespace allspice_vue_csharp.Models;
+
+public class Recipe
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string Title { get; set}
+
+
+}
