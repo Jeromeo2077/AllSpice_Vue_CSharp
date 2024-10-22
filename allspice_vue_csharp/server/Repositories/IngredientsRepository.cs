af9c660b26f242356c73770396edb6568605ba44
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-21 19:53:33 -0600

    Refactor IngredientsRepository to simplify SQL query and improve performance

--- allspice_vue_csharp/server/Repositories/IngredientsRepository.cs
+++ allspice_vue_csharp/server/Repositories/IngredientsRepository.cs
@@ -16,21 +16,12 @@ public class IngredientsRepository
   {
     string sql = @"
 INSERT into 
-ingredients(name, quantity, creatorId, recipeId)
-values(@Name, @Quantity, @CreatorId, @RecipeId);
+ingredients(name, quantity, recipeId)
+values(@Name, @Quantity, @RecipeId);
 
-SELECT
-ingredients.*,
-accounts.*
-From ingredients
-JOIN accounts on ingredients.CreatorId = accounts.id
- WHERE ingredients.id = LAST_INSERT_ID();";
+SELECT * From ingredients WHERE id = LAST_INSERT_ID();";
 
-    Ingredient ingredient = _db.Query<Ingredient, Account, Ingredient>(sql, (ingredient, account) =>
-    {
-      ingredient.Creator = account;
-      return ingredient;
-    }, ingredientData).FirstOrDefault();
+    Ingredient ingredient = _db.Query<Ingredient>(sql, (ingredientData)).FirstOrDefault();
 
     return ingredient;
   }
