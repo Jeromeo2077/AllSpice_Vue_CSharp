b7dd93069f55712337ddd0e3f89d0c629edba295
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-21 20:03:04 -0600

    Refactor IngredientsRepository to fix SQL query syntax and improve performance

--- allspice_vue_csharp/server/Repositories/IngredientsRepository.cs
+++ allspice_vue_csharp/server/Repositories/IngredientsRepository.cs
@@ -17,11 +17,11 @@ public class IngredientsRepository
     string sql = @"
 INSERT into 
 ingredients(name, quantity, recipeId)
-values(@Name, @Quantity, @RecipeId);
+VALUES(@Name, @Quantity, @RecipeId);
 
 SELECT * From ingredients WHERE id = LAST_INSERT_ID();";
 
-    Ingredient ingredient = _db.Query<Ingredient>(sql, (ingredientData)).FirstOrDefault();
+    Ingredient ingredient = _db.Query<Ingredient>(sql, ingredientData).FirstOrDefault();
 
     return ingredient;
   }
